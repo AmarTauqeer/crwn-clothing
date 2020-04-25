f6fa52f03eceb7a895f6edc0d3a7c3f2460ba9cf
unknown <amar.tauqeer@gmail.com>
2020-04-25 16:35:55 +0200

    collection overview and preview completed

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -5,6 +5,7 @@ import storage from 'redux-persist/lib/storage';
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 import driectoryReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducer';
 
 const persitConfig ={
   key : 'root',
@@ -15,7 +16,8 @@ const persitConfig ={
 const rootReducer =combineReducers({
   user: userReducer,
   cart: cartReducer,
-  directory: driectoryReducer
+  directory: driectoryReducer,
+  shop : shopReducer
 });
 
 export default persistReducer(persitConfig,rootReducer);
